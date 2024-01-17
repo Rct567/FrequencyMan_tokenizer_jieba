2aea0529cbb8c8047eaac1df16f377df73849e98
Rick <rct999@gmail.com>
2024-01-17 05:05:53 +0100

    init

---
+++ fm_init_jieba.py
@@ -0,0 +1,13 @@
+
+from typing import Callable
+from jieba import lcut as jieba_lcut, setLogLevel as jieba_set_log_level
+
+jieba_set_log_level('WARNING')
+
+def jieba_tokenizer_provided_via_user_file_init(txt: str) -> list[str]:
+
+    return list(token for token in jieba_lcut(txt) if token is not None)
+
+def jieba_tokenizers_provider() -> list[tuple[str, Callable]]:
+
+    return [("zh", jieba_tokenizer_provided_via_user_file_init)]
