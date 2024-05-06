70ca271e8264da0476246c23517a7ce26f07b5da
Rick Zuidhoek <rct999@gmail.com>
2024-05-07 00:14:56 +0200

    Update fm_init_jieba.py

--- fm_init_jieba.py
+++ fm_init_jieba.py
@@ -4,7 +4,7 @@ from jieba import lcut as jieba_lcut, setLogLevel as jieba_set_log_level
 
 jieba_set_log_level('WARNING')
 
-def jieba_tokenizer_provided_via_user_file_init(txt: str) -> list[str]:
+def jieba_tokenizer_provided_via_user_files_init(txt: str) -> list[str]:
 
     return list(token for token in jieba_lcut(txt) if token is not None)
 
